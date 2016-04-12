6c81f6d271f774407027e2a1324b84aaff4f573b
Le Duc Thien <timducle@yahoo.com>
2016-04-12 20:31:51 +0700

    update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,7 +1,10 @@
 class VisitorsController < ApplicationController
   def new
+    Rails.logger.debug 'Enter VisitorsController new method'
     @owner = Owner.new
     render 'visitors/new'
+    Rails.logger.debug 'Exit VisitorsController new method'
+    
   end
 
 end
--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,13 +1,20 @@
 class Owner
   def name
+    Rails.logger.debug 'Enter Owner new method'
     name = 'Foo'
+    #Rails.logger.debug 'Owner name is ' + name
+    # The last statement in a method will be the return value. In this case the
+    # return value is true
+    #Rails.logger.debug 'Exit Owner new method'
   end
 
   def birthdate
+    # why is it possible to use Ruby API Date class without some require statement?
     birthdate = Date.new(1981, 10, 17)
   end
 
   def countdown
+    Rails.logger.debug 'Enter Owner countdown method'
     today = Date.today
     birthday = Date.new(today.year, birthdate.month, birthdate.day)
     if birthday > today
@@ -15,6 +22,7 @@ class Owner
     else
       countdown = birthday.next_year - today
     end
+    #Rails.logger.debug 'Exit Owner countdown method'
   end
 
 end
